4f1a84beb87e96c74d82ded95d255d95917836d5
jabir9211 <41592882+jabir9211@users.noreply.github.com>
2021-12-19 22:50:46 +0500

    Add files via upload

---
+++ refactoring-challenge-7456c00029477ba209a862db19d820d569b6c234/02-gilded-rose-java/src/main/java/com/gildedrose/GildedRose.java
@@ -0,0 +1,75 @@
+package com.gildedrose;
+
+class GildedRose {
+    public static final String Aged_Brie = "Aged Brie";
+    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+    public static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    public static final String CONJURED = "Conjured Mana Cake";
+    private Item[] items;
+
+    public GildedRose(Item... items) {
+        this.items = items;
+    }
+
+    public void updateQuality() {
+        for (Item item : items) {
+            updateItemQuality(item);
+        }
+    }
+
+    private void updateItemQuality(Item item){
+        boolean isExpired = item.sellIn < 1;
+        int degradeRate = determineDegradeRate(item, isExpired);
+        boolean hasSellByDate = !item.name.equals(SULFURAS);
+        boolean doesDegrade = !item.name.equals(Aged_Brie) && !item.name.equals(BACKSTAGE_PASSES) && !item.name.equals(SULFURAS);
+
+        if  (item.name.equals(Aged_Brie)){
+            int adjustment = isExpired ? 2 : 1;
+            adjustQuality(item,adjustment);
+        }
+
+        if (doesDegrade) {
+            adjustQuality(item, degradeRate);
+            }
+
+        if (item.name.equals(BACKSTAGE_PASSES)) {
+            updateBackstagePassQuality(item, isExpired);
+        }
+
+        if (hasSellByDate) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void updateBackstagePassQuality(Item item, boolean isExpired) {
+        adjustQuality(item,  1);
+        if (item.sellIn < 11) {
+            adjustQuality(item, 1);
+        }
+
+        if (item.sellIn < 6) {
+            adjustQuality(item, 1);
+        }
+        if (isExpired) {
+            item.quality = item.quality - item.quality;
+        }
+    }
+
+    private int determineDegradeRate(Item item, boolean isExpired) {
+        int baseDegradeRate = item.name.equals(CONJURED) ? -2 : -1;
+        return isExpired ?  baseDegradeRate *2 : baseDegradeRate;
+    }
+
+    private void adjustQuality(Item item, int adjustment) {
+        int newQuality = item.quality +adjustment;
+        boolean inValidRange = newQuality <= 50 && newQuality >= 0;
+        if (inValidRange){
+            item.quality = newQuality;
+        }
+
+    }
+
+    public Item[] getItems() {
+        return items;
+    }
+}
